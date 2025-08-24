c9196a877a2f4f187f35b5379a32e424c046d519
asier-vega-gutierrez <asiervg@outlook.com>
2025-08-24 13:57:46 +0200

    compare db api done

--- src/db_comparer/src/config/config.py
+++ src/db_comparer/src/config/config.py
@@ -6,5 +6,6 @@ class ApplicationConfiguration(metaclass=SingletonMeta):
     def __init__(self, username:str):
 
         # APPLICATION CONFIGURATION
-        self.APP_RECEIVED_DB_PATH= f'./upload/{username}_received.db'
-        self.APP_STORED_DB_PATH = f'./upload/{username}_stored.db'
+        self.APP_UPLOAD_FOLDER = './upload'
+        self.APP_RECEIVED_DB_PATH = f'./upload/{username}_local.db'
+        self.APP_STORED_DB_PATH = f'./upload/{username}_cloud.db'
--- src/db_comparer/src/main.py
+++ src/db_comparer/src/main.py
@@ -6,33 +6,30 @@ import os
 from flask import Flask, jsonify, request
 
 
-UPLOAD_FOLDER = './upload'
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-config = ApplicationConfiguration('asier')
 
 
-@app.route('/upload_local', methods=['POST'])
+# Call this method to upload the db files
+@app.route('/upload', methods=['POST'])
 def upload():
 
     if request.method == 'POST':
 
-        file = request.files['file']
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], "asier_local.db"))
+        # Get the aeguments
+        type = request.args.get('type')
+        username = request.args.get('username')
+        if(username != ""):
+            config = ApplicationConfiguration(username)
 
-        return f"File {file.filename} uploaded successfully \n", 200
+            # Save the file as wanted name
+            file = request.files['file']
+            file.save(os.path.join(config.APP_UPLOAD_FOLDER, f"{username}_{type}.db"))
 
-
-@app.route('/upload_cloud', methods=['POST'])
-def upload():
-
-    if request.method == 'POST':
-
-        file = request.files['file']
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], "asier_cloud.db"))
-
-        return f"File {file.filename} uploaded successfully \n", 200
+            # Send the respose back
+            return f"File {file.filename} uploaded successfully \n", 200
+        
+        else:
+            return "No username provided", 400
 
 # Call this method to compare the two actual dbs (must be uploaded firts)
 @app.route('/compare', methods=['GET'])
@@ -40,28 +37,35 @@ def compare():
 
     if request.method == 'GET':
         
-        # Read the bd information
-        local_db = Sqlite(config.APP_RECEIVED_DB_PATH)
-        cloud_db = Sqlite(config.APP_STORED_DB_PATH)
+        username = request.args.get('username')
 
-        # Compare the ids to find diferences
-        ids_to_uploada = pd.DataFrame(data = (set(local_db.data[0].values) - set(cloud_db.data[0].values)) ) 
-        #print(ids_to_uploada)
+        if(username != ""):
+            config = ApplicationConfiguration(username)
 
-        # Save the response
-        with open("output/ids.json", "w+") as json_file:
-            json.dump(ids_to_uploada.to_json(), json_file)
-        
-        # Send the respose back
-        return jsonify(ids_to_uploada.to_json()), 200
+            # Read the bd information
+            local_db = Sqlite(config.APP_RECEIVED_DB_PATH)
+            cloud_db = Sqlite(config.APP_STORED_DB_PATH)
+
+            # Compare the ids to find diferences
+            ids_to_uploada = pd.DataFrame(data = (set(local_db.data[0].values) - set(cloud_db.data[0].values)) ) 
+            #print(ids_to_uploada)
+
+            # Save the response
+            with open("output/ids.json", "w+") as json_file:
+                json.dump(ids_to_uploada.to_json(), json_file)
+            
+            # Send the respose back
+            return jsonify(ids_to_uploada.to_json()), 200
+        else:
+            return "No username provided", 400
 
 
     
 # Test GET request
-#curl -X GET http://localhost:5000/compare
+#curl -X GET http://localhost:5000/compare?username=asier
 
 # Test POST request with file
-#curl -X POST -F "file=@/home/asier/Personal_cloud/src/local_sync_client/db/tracker.db" http://localhost:5000/upload
+#curl -X POST -F "file=@/home/asier/Personal_cloud/src/sync_client/db/tracker.db" "http://localhost:5000/upload?type=local&username=asier"
 
 
 if __name__ == "__main__":
